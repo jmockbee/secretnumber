d1601ec86907281cf7e44ffe2f4cc4bf62214654
Jon Mockbee <mockbee1000@gmail.com>
2021-07-27 22:12:17 -0400

    stil working on it

--- secretnumberfile.py
+++ secretnumberfile.py
@@ -4,25 +4,20 @@ print(
 +================================+
 | Welcome to my game, muggle!    |
 | Enter an 3 digit integer number|
-| and guess what number I've     |
+| to guess what number I've     |
 | picked for you.                |
-| So, what is the secret_number number? |
+| So, what is the secret number? |
 +================================+
 """)
 
 
 secret_number = 777
 
-
 guess = int(input("Enter your number: "))
 
-
-
 while guess != secret_number:
-        print ("try your guess again .")
-
-        guess = int(input("Take a guess.\n")
-
-    else:
-        
-        print ("Good job:", secret_number )
+    if (guess > secret_number):
+      print ("Wrong! You guessed too high")
+    if (guess < secret_number):
+      print ("Wrong! You guessed too low")
+    print ("Good job",secret_number )
