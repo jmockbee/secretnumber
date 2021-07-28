f2642ad428f09882a382b50ed89214d9ca946f7f
Jon Mockbee <mockbee1000@gmail.com>
2021-07-28 17:45:10 -0400

    its not ending

--- secretnumberfile.py
+++ secretnumberfile.py
@@ -12,12 +12,8 @@ print(
 
 
 secret_number = 777
-
 guess = int(input("Enter your number: "))
-
 while guess != secret_number:
-    if (guess > secret_number):
-      print ("Wrong! You guessed too high")
-    if (guess < secret_number):
-      print ("Wrong! You guessed too low")
+      print ("Wrong! try again")
+else:
     print ("Good job",secret_number )
