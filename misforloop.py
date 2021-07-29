ce5b0dc04b592a5e5a97333a7f804c83e1fb8482
Jon Mockbee <mockbee1000@gmail.com>
2021-07-29 10:46:39 -0400

    the last print message must be outside the loop

--- misforloop.py
+++ misforloop.py
@@ -2,7 +2,7 @@ import time
 
 # Write a for loop that counts to five.
 for i in range(1,6):
-    print( i, "missippi")
+    print( i, "mississippi")
     time.sleep(1)
-
+print ("no more counting")
 # Write a print function with the final message.
